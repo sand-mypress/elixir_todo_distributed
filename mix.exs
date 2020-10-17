4c22da76ee5b689f42d1db2c250301ba494a6b90
sand <sand.mypress@gmail.com>
2020-10-17 16:36:36 +0900

    add Todo.Web module

--- mix.exs
+++ mix.exs
@@ -22,7 +22,10 @@ defmodule Todo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:poolboy, "~> 1.5"}
+      {:poolboy, "~> 1.5"},
+      {:cowboy, "~> 1.1"},
+      {:plug, "~> 1.4"},
+      {:plug_cowboy, "~> 1.0"}
     ]
   end
 end
